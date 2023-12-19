12c06e81d8bd2f612fbc36167a3d20603e12c808
risgandar <98710597+risgandar@users.noreply.github.com>
2023-12-19 20:27:39 +0100

    Update Stock_Dashboard.py

--- Stock_Dashboard.py
+++ Stock_Dashboard.py
@@ -1,10 +1,11 @@
 import streamlit as st, pandas as pd, numpy as np, yfinance as yf
 import plotly.express as px
+import datetime
 
 
 st.title ('Stock Dashboard')
-ticker = st.sidebar.text_input('Ticker')
-start_date = st.sidebar.date_input('Start Date')
+ticker = st.sidebar.text_input('Ticker', 'TSL')
+start_date = st.sidebar.date_input('Start Date', datetime.date(2021, 1, 1))
 end_date = st.sidebar.date_input('End Date')
 
 data = yf.download(ticker, start=start_date, end=end_date)
